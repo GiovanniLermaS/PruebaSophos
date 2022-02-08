aa7c26ecbffff5bdad47bd9a4d9153d6d0b29ead
Giovanni Lerma Suarez <giosuarezler@gmail.com>
2022-02-08 13:02:55 -0500

    View binding, tabs layout and adapter added to the main activity.

--- app/src/main/java/com/example/testing/view/mainactivity/MainActivity.kt
+++ app/src/main/java/com/example/testing/view/mainactivity/MainActivity.kt
@@ -1,12 +1,57 @@
 package com.example.testing.view.mainactivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.testing.R
+import com.example.testing.databinding.ActivityMainBinding
+import com.example.testing.view.mainactivity.adapters.TabLayoutAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.TabLayoutOnPageChangeListener
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        val adapter = TabLayoutAdapter(this, supportFragmentManager, binding?.tl?.tabCount)
+        binding?.vp?.adapter = adapter
+        binding?.vp?.addOnPageChangeListener(TabLayoutOnPageChangeListener(binding?.tl))
+        binding?.tl?.setOnTabSelectedListener(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_activity_actions, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_reload -> {
+                Toast.makeText(this, "Si funciona me lo quedo", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onTabSelected(tab: TabLayout.Tab?) {
+        tab?.let { binding?.vp?.currentItem = it.position }
+    }
+
+    override fun onTabUnselected(tab: TabLayout.Tab?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onTabReselected(tab: TabLayout.Tab?) {
+        TODO("Not yet implemented")
     }
 }
---
+++ app/src/main/java/com/example/testing/view/mainactivity/adapters/TabLayoutAdapter.kt
@@ -0,0 +1,17 @@
+package com.example.testing.view.mainactivity.adapters
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class TabLayoutAdapter(context: Context, fragmentManager: FragmentManager, totalTabs: Int?) :
+    FragmentPagerAdapter(fragmentManager) {
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(position: Int): Fragment {
+        TODO("Not yet implemented")
+    }
+}
