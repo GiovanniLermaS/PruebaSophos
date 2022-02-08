35da8768d7d022a6061e1fc9643b1566c1c0667f
Giovanni Lerma Suarez <giosuarezler@gmail.com>
2022-02-08 11:48:08 -0500

    Main activity added.

--- app/src/main/java/com/example/testing/view/mainactivity/MainActivity.kt
+++ app/src/main/java/com/example/testing/view/mainactivity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.testing
+package com.example.testing.view.mainactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testing.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
